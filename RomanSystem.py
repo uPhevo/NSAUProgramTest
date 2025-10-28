8a0f443404ba98e8a24e852862a6c318eb1330b9
James Carpenter <elklemente777@gmail.com>
2025-10-28 09:52:37 +0700

    first commit

---
+++ RomanSystem.py
@@ -0,0 +1,20 @@
+'''
+Input: nums = [1,0,-1,0,-2,2], target = 0
+Output: [[2,-1,1,-2],[2,0,0,-2],[-1,0,0,1]]
+'''
+nums = [int(num) for num in input('nums = ').split(' ')]
+
+nums = []
+for num in input("nums = ").split(' '):
+       nums.append(int(num))
+
+target = int(input("target = "))
+res = []
+for i in range(len(nums)-3):
+       for j in range(i+1, len(nums)-2):
+              for k in range(j+1, len(nums)-1):
+                     for n in range(k+1,len(nums)):
+                            if((nums[i] + nums[j] + nums[k] + nums[n]) == target and
+                               sorted([nums[i], nums[j], nums[k], nums[n]]) not in res):
+                                   res.append(sorted([nums[i], nums[j], nums[k], nums[n]]))
+print(res)
